6836489ca579e745e5a1e13ad7316161b47f7749
fealtran <efeifotoel1921@gmail.com>
2025-08-04 22:35:40 -0300

    Add files via upload

---
+++ Analise_Mercado_Financeiro_1/app.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+st.set_page_config(page_title="Analyst's Cockpit", page_icon="🚀", layout="wide")
+st.title("Bem-vindo ao Analyst's Cockpit 🚀")
+st.markdown("Utilize a barra lateral à esquerda para navegar entre as ferramentas de análise.")
+st.info("Para começar, selecione uma página na barra de navegação à esquerda.", icon="👈")
